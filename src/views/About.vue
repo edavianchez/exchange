8ad822a9494a47e7070f9d26095fb0e270b8f11f
David Sanchez <e.david.sanchez.b@gmail.com>
2021-02-14 20:16:48 -0500

    Fetch y API de Coincap

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,8 @@
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl">Exchange v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
-      Proyecto para obtener las cotizaciones de las cryptomonedas más importantes a través de la API de Coincap.
+      Proyecto para obtener las cotizaciones de las cryptomonedas más
+      importantes a través de la API de Coincap.
       <br />Este proyecto es utilizado en el curso de Vue.js 2 Basico de Platzi.
     </p>
     <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
